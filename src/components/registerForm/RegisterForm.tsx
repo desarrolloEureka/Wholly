571316d1d107da64d5cc4781a8c3dd30859f1950
juancapera26 <caperajuandavid26@gmail.com>
2025-02-13 08:41:06 -0500

    input_titulo

--- src/components/registerForm/RegisterForm.tsx
+++ src/components/registerForm/RegisterForm.tsx
@@ -1,163 +1,121 @@
-import { useTranslation } from 'react-i18next';
-import { useState } from 'react';
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
 import {
   Box,
   Button,
   useTheme,
   MobileStepper,
   Typography,
-} from '@mui/material';
-import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
-import { StepOne } from './components/StepOne';
-import { OptionsButtons } from '../../globals/types';
-import { ContentStepper } from './components/contentStepper/ContentStepper';
-import { ContentStepperSpecial } from './components/contentStepperSpecial/ContentStepperSpecial';
-import { InteractiveText } from '../../globals/elements';
+} from "@mui/material";
+import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
+import { StepOne } from "./components/StepOne";
+import { OptionsButtons } from "../../globals/types";
+import { ContentStepper } from "./components/contentStepper/ContentStepper";
+import { ContentStepperSpecial } from "./components/contentStepperSpecial/ContentStepperSpecial";
+import { InteractiveText } from "../../globals/elements";
 
 const RegisterForm = () => {
   const theme = useTheme();
   const { t } = useTranslation();
   const [activeStep, setActiveStep] = useState(0);
   const options2: OptionsButtons[] = [
-    {
-      id: 1,
-      name: 'name button',
-    },
-    {
-      id: 2,
-      name: 'name button',
-    },
-    {
-      id: 3,
-      name: 'name button',
-    },
-    {
-      id: 4,
-      name: 'name button',
-    },
-    {
-      id: 5,
-      name: 'name button',
-    },
-    {
-      id: 6,
-      name: 'name button',
-    },
-    {
-      id: 7,
-      name: 'name button',
-    },
-    {
-      id: 8,
-      name: 'name button',
-    },
-    {
-      id: 9,
-      name: 'name button',
-    },
-    {
-      id: 10,
-      name: 'name button',
-    },
-    {
-      id: 11,
-      name: 'name button',
-    },
-    {
-      id: 12,
-      name: 'name button',
-    },
-    {
-      id: 13,
-      name: 'name button',
-    },
-    {
-      id: 14,
-      name: 'name button',
-    },
-    {
-      id: 15,
-      name: 'name button',
-    },
-    {
-      id: 16,
-      name: 'name button',
-    },
+    { id: 1, name: "Oily hair" },
+    { id: 2, name: "Dry hair" },
+    { id: 3, name: "Thin hair" },
+    { id: 4, name: "Hair loss" },
+    { id: 5, name: "A lot of hair" },
+    { id: 6, name: "Lorem Impsum Hair" },
+    { id: 7, name: "Lorem Impsum Hair" },
+    { id: 8, name: "Lorem Impsum Hair" },
+    { id: 9, name: "Lorem Impsum Hair" },
+    { id: 10, name: "Lorem Impsum Hair" },
+    { id: 11, name: "Lorem Impsum Hair" },
+    { id: 12, name: "Lorem Impsum Hair" },
+    { id: 13, name: "Lorem Impsum Hair" },
+    { id: 14, name: "Lorem Impsum Hair" },
+    { id: 15, name: "Lorem Impsum Hair" },
+    { id: 16, name: "Lorem Impsum Hair" },
   ];
 
   const options3: OptionsButtons[] = [
     {
       id: 1,
-      name: 'name button',
+      name: "Walnuts  ",
     },
     {
       id: 2,
-      name: 'name button',
+      name: "Brazil nuts",
     },
   ];
 
   const options4: OptionsButtons[] = [
     {
       id: 1,
-      name: 'name button',
+      name: "Sertraline",
     },
   ];
 
   const options5: OptionsButtons[] = [
     {
       id: 1,
-      name: 'name button',
+      name: "Epilepsy",
     },
   ];
   // Definimos los pasos correctamente
   const steps = [
     {
-      label: 'Select campaign settings',
+      label: "Select campaign settings",
       description: <StepOne />,
     },
     {
-      label: '2',
+      label: "2",
       description: <ContentStepperSpecial />,
     },
     {
-      label: '3',
+      label: "3",
       description: (
         <ContentStepper
-          title1='select the '
-          title2='conditions '
-          title3='you have'
+          title1="select the "
+          title2="conditions "
+          title3="you have"
           ButtonsOptions={options2}
-          description='Enter the name of what you are looking for as many times as you need and add them one by one.'
+          description="Enter the name of what you are looking for as many times as you need and add them one by one."
         />
       ),
     },
     {
-      label: '4',
+      label: "4",
       description: (
         <ContentStepper
-          title1='select the allergies you have'
+          title1="select the "
+          title2="allergies "
+          title3="you have"
           ButtonsOptions={options3}
-          description='Enter the name of what you are looking for as many times as you need and add them one by one.'
+          description="Enter the name of what you are looking for as many times as you need and add them one by one."
         />
       ),
     },
     {
-      label: '5',
+      label: "5",
       description: (
         <ContentStepper
-          title1='select the medicines you have'
+          title1="select the "
+          title2="medicines "
+          title3="you have"
           ButtonsOptions={options4}
-          description='Enter the name of what you are looking for as many times as you need and add them one by one.'
+          description="Enter the name of what you are looking for as many times as you need and add them one by one."
         />
       ),
     },
     {
-      label: '6',
+      label: "6",
       description: (
         <ContentStepper
-          title1='select the diseases you have'
+          title1="select the "
+          title2="diseases "
+          title3="you have"
           ButtonsOptions={options5}
-          description='Enter the name of what you are looking for as many times as you need and add them one by one.'
+          description="Enter the name of what you are looking for as many times as you need and add them one by one."
         />
       ),
     },
@@ -177,49 +135,49 @@ const RegisterForm = () => {
     <Box
       sx={{
         flexGrow: 1,
-        width: '100%',
-        margin: '0 auto',
-        padding: '30px',
+        width: "100%",
+        margin: "0 auto",
+        padding: "30px",
         background:
-          'linear-gradient(to left,rgba(238, 244, 252, 0.97), #EEF1F02B)', // Gradiente de azul claro a transparente
-        borderRadius: '20px',
-        backdropFilter: 'blur(4px)', // Efecto de desenfoque
-        boxShadow: ' 0px 5px 16px rgba(0, 0, 0, 0.4)',
+          "linear-gradient(to left,rgba(238, 244, 252, 0.97), #EEF1F02B)", // Gradiente de azul claro a transparente
+        borderRadius: "20px",
+        backdropFilter: "blur(4px)", // Efecto de desenfoque
+        boxShadow: " 0px 5px 16px rgba(0, 0, 0, 0.4)",
       }}
     >
-      <Box sx={{ textAlign: 'end', mt: 1, paddingRight: 2 }}>
+      <Box sx={{ textAlign: "end", mt: 1, paddingRight: 2 }}>
         <InteractiveText
           // onClick={() => navigate("/remember")}
           style={{
-            textDecoration: 'underline',
-            fontFamily: 'sans-serif',
-            fontSize: '0.845rem',
-            cursor: 'pointer',
+            textDecoration: "underline",
+            fontFamily: "sans-serif",
+            fontSize: "0.845rem",
+            cursor: "pointer",
           }}
         >
-          {t('registerForm.later')}
+          {t("registerForm.later")}
         </InteractiveText>
       </Box>
-      <Box sx={{ width: '100%', minHeight: 400, p: 2 }}>
+      <Box sx={{ width: "100%", minHeight: 400, p: 2 }}>
         {steps[activeStep].description}
       </Box>
       <MobileStepper
-        variant='text'
+        variant="text"
         steps={maxSteps}
-        position='static'
+        position="static"
         activeStep={activeStep}
         sx={{
-          backgroundColor: 'transparent',
+          backgroundColor: "transparent",
         }}
         nextButton={
           <Button
-            size='small'
+            size="small"
             onClick={handleNext}
             disabled={activeStep === maxSteps - 1}
           >
-            <Typography>{t('stepper.nextButton')}</Typography>
+            <Typography>{t("stepper.nextButton")}</Typography>
 
-            {theme.direction === 'rtl' ? (
+            {theme.direction === "rtl" ? (
               <KeyboardArrowLeft />
             ) : (
               <KeyboardArrowRight />
@@ -227,13 +185,13 @@ const RegisterForm = () => {
           </Button>
         }
         backButton={
-          <Button size='small' onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === "rtl" ? (
               <KeyboardArrowRight />
             ) : (
               <KeyboardArrowLeft />
             )}
-            <Typography>{t('stepper.backButton')}</Typography>
+            <Typography>{t("stepper.backButton")}</Typography>
           </Button>
         }
       />
