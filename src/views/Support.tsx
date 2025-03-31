e6ffc53223e3f3d7bea5088e1f2da02ddd6961ff
juancapera26 <caperajuandavid26@gmail.com>
2025-03-30 22:24:29 -0500

    nuevo soporte

--- src/views/Support.tsx
+++ src/views/Support.tsx
@@ -1,17 +1,42 @@
 import { Box, Typography } from "@mui/material";
 import CustomAppBar from "../components/customAppBar/CustomAppBar";
 import FooterApp from "../components/footerApp/FooterApp";
+import { CustomerService } from "../components/supportForm/CustomerService";
 
 export const Support = () => {
   return (
-    <Box justifyContent="space-between">
-      <Box className="bg_Blog">
+    <Box display="flex" flexDirection="column" minHeight="90vh">
+      {/* Contenedor del AppBar y contenido */}
+      <Box
+        className="bg_support"
+        display="flex"
+        flexDirection="column"
+        flexGrow={1}
+      >
         <CustomAppBar />
-      </Box>
-      <Typography sx={{ transform: "translate(-40%, -90%)" }}>
-        holaffffffffff
-      </Typography>
 
+        <Box
+          display="flex"
+          flexGrow={1}
+          justifyContent="flex-start"
+          alignItems="flex-end"
+          pb={2}
+        >
+          <Typography
+            variant="h6"
+            borderBottom="1px solid #3C3C3C"
+            sx={{
+              padding: "15px",
+              width: "30%",
+              color: "#3C3C3C",
+              fontSize: "30px",
+            }}
+          >
+            Support
+          </Typography>
+        </Box>
+      </Box>
+      <CustomerService />
       <FooterApp />
     </Box>
   );
