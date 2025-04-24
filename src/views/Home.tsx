006132ee865aeda12bbc274b23e900907d385e4e
rodacapera <rodacapera@gmail.com>
2025-04-23 21:54:42 -0500

    qa3

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,36 +1,38 @@
-import { Box, Container, Typography } from "@mui/material";
-import CustomAppBar from "../components/customAppBar/CustomAppBar";
-import FooterApp from "../components/footerApp/FooterApp";
-import { HomeSupplement } from "../components/homeForm/components/homeSupplement/HomeSupplement";
-import { RegisterHome } from "../components/homeForm/components/registerHome/RegisterHome";
-import { HomeExclusive } from "../components/homeForm/components/homeExclusive/HomeExclusive";
-import { HomeCare } from "../components/homeForm/components/homeCare/HomeCare";
-import { HomeCategories } from "../components/homeForm/components/homeCategories/HomeCategories";
-import { Homevariety } from "../components/homeForm/components/homeVariety/HomeVariety";
-import { useTranslation } from "react-i18next";
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import CustomAppBar from '../components/customAppBar/CustomAppBar';
+import FooterApp from '../components/footerApp/FooterApp';
+import { HomeSupplement } from '../components/homeForm/components/homeSupplement/HomeSupplement';
+import { RegisterHome } from '../components/homeForm/components/registerHome/RegisterHome';
+import { HomeExclusive } from '../components/homeForm/components/homeExclusive/HomeExclusive';
+import { HomeCare } from '../components/homeForm/components/homeCare/HomeCare';
+import { HomeCategories } from '../components/homeForm/components/homeCategories/HomeCategories';
+import { Homevariety } from '../components/homeForm/components/homeVariety/HomeVariety';
+import { useTranslation } from 'react-i18next';
 
 export const Home = () => {
   const { t } = useTranslation();
 
   return (
-    <Box justifyContent="space-between">
-      <Box className="bg_Home_image">
+    <Box justifyContent='space-between'>
+      <Box className='bg_Home_image'>
         <CustomAppBar />
         <Container
-          component="main"
+          component='main'
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "80vh",
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '80vh',
             width: {
-              xs: "90%", // 90% del ancho en pantallas extra pequeñas
-              sm: "80%", // 70% del ancho en pantallas pequeñas
-              md: "50%", // 50% del ancho en pantallas medianas
-              lg: "40%", // 40% del ancho en pantallas grandes
-              xl: "38%", // 38% del ancho en pantallas extra grandes
+              xs: '90%', // 90% del ancho en pantallas extra pequeñas
+              sm: '80%', // 70% del ancho en pantallas pequeñas
+              md: '50%', // 50% del ancho en pantallas medianas
+              lg: '40%', // 40% del ancho en pantallas grandes
+              xl: '38%', // 38% del ancho en pantallas extra grandes
             },
-            marginRight: { sm: "10px", md: "100px" }, // Mueve el contenedor hacia la derecha
+            marginRight: { sm: '10px', md: '100px' }, // Mueve el contenedor hacia la derecha
           }}
         >
           <HomeSupplement />
@@ -41,22 +43,22 @@ export const Home = () => {
 
       <Box
         sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
         <Typography
-          variant="h1"
+          variant='h1'
           sx={{
-            marginTop: "5%",
-            marginBottom: "3%",
-            fontWeight: "bold",
-            color: "#3C3C3C",
-            fontSize: "2.1rem",
+            marginTop: '5%',
+            marginBottom: '3%',
+            fontWeight: 'bold',
+            color: '#3C3C3C',
+            fontSize: '2.1rem',
           }}
         >
-          {t("homeform.Variety")}
+          {t('homeform.Variety')}
         </Typography>
       </Box>
       <Homevariety />
--- src/views/InternalCategoriesty.tsx
+++ src/views/InternalCategoriesty.tsx
@@ -1,88 +1,89 @@
-import { Box, Typography } from "@mui/material";
-import CustomAppBar from "../components/customAppBar/CustomAppBar";
-import FooterApp from "../components/footerApp/FooterApp";
-import { HomeCategories } from "../components/homeForm/components/homeCategories/HomeCategories";
-import { HomeExclusive } from "../components/homeForm/components/homeExclusive/HomeExclusive";
-import { InternalCategoriestyform } from "../components/categoriesForm.ts/componets/internalCategoriestyform/InternalCategoriestyform";
-import { useTranslation } from "react-i18next";
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import CustomAppBar from '../components/customAppBar/CustomAppBar';
+import FooterApp from '../components/footerApp/FooterApp';
+import { HomeCategories } from '../components/homeForm/components/homeCategories/HomeCategories';
+import { HomeExclusive } from '../components/homeForm/components/homeExclusive/HomeExclusive';
+import { InternalCategoriestyform } from '../components/categoriesForm.ts/componets/internalCategoriestyform/InternalCategoriestyform';
+import { useTranslation } from 'react-i18next';
 
 export const InternalCategoriesty = () => {
   const { t } = useTranslation();
 
   return (
-    <Box justifyContent="space-between">
-      <Box className="bg_InternalCategoriesty_image">
+    <Box justifyContent='space-between'>
+      <Box className='bg_InternalCategoriesty_image'>
         <CustomAppBar />
         <Typography
-          variant="h2"
+          variant='h2'
           sx={{
-            textAlign: "start",
-            width: "50%",
-            mt: "9%",
-            marginLeft: "50px",
-            fontSize: "2.6rem",
+            textAlign: 'start',
+            width: '50%',
+            mt: '9%',
+            marginLeft: '50px',
+            fontSize: '2.6rem',
             fontWeight: 100,
-            color: "#Ffff",
+            color: '#Ffff',
           }}
         >
-          {t("categoriesIternal.supplementsSkinCare")}
+          {t('categoriesIternal.supplementsSkinCare')}
         </Typography>
       </Box>
 
       <Box
         sx={{
-          display: "flex",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          marginLeft: "90px",
+          display: 'flex',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          marginLeft: '90px',
         }}
       >
         <Typography
-          variant="h3"
+          variant='h3'
           sx={{
-            marginTop: "5%",
-            marginBottom: "3%",
-            fontWeight: "bold",
-            color: "#3C3C3C",
-            fontSize: "2.1rem",
+            marginTop: '5%',
+            marginBottom: '3%',
+            fontWeight: 'bold',
+            color: '#3C3C3C',
+            fontSize: '2.1rem',
           }}
         >
-          {t("categoriesIternal.SkinCare")}
+          {t('categoriesIternal.SkinCare')}
         </Typography>
       </Box>
       <InternalCategoriestyform />
 
       <Box
         sx={{
-          backgroundColor: "#E8E4DE",
+          backgroundColor: '#E8E4DE',
         }}
       >
         <HomeExclusive />
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: " rgba(28, 28, 28, 0.7)",
-            height: "2px",
-            width: "90%",
-            margin: "auto",
-            marginTop: "6%",
-            marginBottom: "8%",
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: ' rgba(28, 28, 28, 0.7)',
+            height: '2px',
+            width: '90%',
+            margin: 'auto',
+            marginTop: '6%',
+            marginBottom: '8%',
           }}
         ></Box>
 
         <Typography
-          variant="h3"
+          variant='h3'
           sx={{
-            marginBottom: "6%",
-            fontWeight: "bold",
-            ml: "60px",
-            color: "#3C3C3C",
-            fontSize: "2.1rem",
+            marginBottom: '6%',
+            fontWeight: 'bold',
+            ml: '60px',
+            color: '#3C3C3C',
+            fontSize: '2.1rem',
           }}
         >
-          {t("categoriesIternal.YouLike")}
+          {t('categoriesIternal.YouLike')}
         </Typography>
         <HomeCategories />
       </Box>
