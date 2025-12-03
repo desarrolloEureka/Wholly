b88ad0de5f1de5a198dbbd10fdffc891ff2b9f69
Diego Peralta <diegoperalta998@gmail.com>
2025-12-03 13:21:01 -0500

    diego entrega 3 dic merge v6 fixes

--- src/components/internalBlog/components/CareBlog.tsx
+++ src/components/internalBlog/components/CareBlog.tsx
@@ -100,7 +100,7 @@ export const CareBlog = ({ blog, loading }: any) => {
               {new Intl.DateTimeFormat(
                 currentLang === "es" ? "es-ES" : "en-US",
                 { day: "numeric", month: "long", year: "numeric" }
-              ).format(new Date(blog.created_at))}
+              ).format(new Date(blog?.created_at))}
             </Typography>
           </Box>
         </Box>
